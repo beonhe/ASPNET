a25c12090fdf945c55817bb51ec824e01d6f091d
Administrator <Administrator@PC245>
2020-06-04 14:10:10 +0700

    Buoi 1.1

---
+++ NguyenVanThuan_lab456/Controllers/CourseController.cs
@@ -0,0 +1,29 @@
+﻿using NguyenVanThuan_lab456.Models;
+using NguyenVanThuan_lab456.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NguyenVanThuan_lab456.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public CourseController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        // GET: Course
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
